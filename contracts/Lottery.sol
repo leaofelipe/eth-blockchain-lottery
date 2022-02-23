b16b9fdcaae08e6566c9b345cd3c67062de90cb3
Felipe Leão <felipe@felipeleao.com>
2022-02-22 21:50:52 -0300

    Base contract structure

---
+++ contracts/Lottery.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+  address public manager;
+  address[] public players;
+
+  function Lottery() public {
+    manager = msg.sender;
+  }
+
+  function enter() public payable {
+    //Validation - value in WEI converted to ETH
+    require(msg.value > .01 ether);
+
+    players.push(msg.sender);
+  }
+
+  function random() private view returns (uint256) {
+    return uint256(keccak256(block.difficulty, now, players));
+  }
+
+  function pickWinner() public {
+    uint256 index = random() % players.length;
+    players[index].transfer(this.balance); //instance of a contract
+    players = new address[](0);
+  }
+}
